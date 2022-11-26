52ee3f0b697c8c6c62684f6ebf20a214417261e5
93294 <9329496@mail.ru>
2022-11-26 20:02:41 +0300

    Initial commit

---
+++ src/main/java/com/homework/fundamentalsofalgorithmsparttwo/FundamentalsOfAlgorithmsPartTwoApplication.java
@@ -0,0 +1,13 @@
+package com.homework.fundamentalsofalgorithmsparttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPartTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPartTwoApplication.class, args);
+    }
+
+}
